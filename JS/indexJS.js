2f1728c6c2aa88cf677b5a4cc10c0201b5264433
Nero <anotonbiserov@gmail.com>
2020-04-02 15:09:42 +0700

    spez 90%

--- JS/indexJS.js
+++ JS/indexJS.js
@@ -1,24 +1,27 @@
 var open = document.getElementById("but1");
 var close = document.getElementById("but2");
 var menuBoard = document.getElementById("Menu-Board");
+
 var speedFrame = 1;
 
+
+//Menu board
 open.onclick = function()
 {
-  var pos = -250;
+  var pos = 960;
   var id = setInterval(frame, speedFrame);
 
 
     function frame()
     {
-      if (pos >= -12)
+      if (pos >= -10)
       {
         clearInterval(id);
       }
       if (menuBoard.style.display != "none")
       {
-        pos+=10;
-        menuBoard.style.marginLeft = pos + 'px';
+        pos+=5;
+        menuBoard.style.transform = 'translateX('+pos+'px)';
       }
     }
 }
@@ -29,14 +32,14 @@ close.onclick = function() {
 
   function frame()
   {
-    if (pos <= -291)
+    if (pos <= -900)
     {
       clearInterval(id);
     }
     if (menuBoard.style.display != "none")
     {
-      pos-=5;
-      menuBoard.style.marginLeft = pos + 'px';
+      pos-=25;
+      menuBoard.style.transform = 'translateX('+pos+'px)';
     }
   }
 }
